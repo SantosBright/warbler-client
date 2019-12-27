8dbf49739d0b7dcfe8a6fa0fd73d3ca31a8adca7
SantosBright <itzsantosbright15@gmail.com>
2019-12-27 23:38:02 +0100

    set up store and App.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Pet from './Pet';
+import { App } from './containers/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Pet />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
