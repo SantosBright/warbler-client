f545ba1655b158f9fa13d2c1566ca816c90b0d03
SantosBright <itzsantosbright15@gmail.com>
2019-12-28 21:34:52 +0100

    style Homepage

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { configureStore } from '../store';
 import { BrowserRouter as Router } from 'react-router-dom';
+import Main from './Main';
 import Navbar from './Navbar';
 
 const store = configureStore();
@@ -12,6 +13,7 @@ const App = () => {
       <Router>
         <div className="onboarding">
           <Navbar />
+          <Main />
         </div>
       </Router>
     </Provider>
